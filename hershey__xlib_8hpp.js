387a8c0bea16a38d438d223e7aca31913c7ed102
dc2917 <dc2917@users.noreply.github.com>
2025-03-16 13:30:54 +0000

    deploy: 1480c9b6d7ab5409a9fe4cbb063315c58a33f039

--- hershey__xlib_8hpp.js
+++ hershey__xlib_8hpp.js
@@ -1,7 +1,7 @@
 var hershey__xlib_8hpp =
 [
     [ "draw_font", "hershey__xlib_8hpp.html#a1adee9fd392f170534b86c95e07baf25", null ],
-    [ "draw_glyph", "hershey__xlib_8hpp.html#afecc775bd1a8ee515b4954724181a472", null ],
-    [ "draw_glyphs", "hershey__xlib_8hpp.html#a543b2c7f6f887a21da625364187c1303", null ],
+    [ "draw_glyph", "hershey__xlib_8hpp.html#aa6872c809827648ff8bfd8ecfb918073", null ],
+    [ "draw_glyphs", "hershey__xlib_8hpp.html#a6161c093156cc4a7f4a72efe6b376cd1", null ],
     [ "position", "hershey__xlib_8hpp.html#aecf1727dd67f7d4f3d9a20c94c0d7cf9", null ]
 ];
